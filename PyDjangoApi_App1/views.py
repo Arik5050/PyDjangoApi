60670bcc691e0293e879d4cd732f51b68ec7c82c
Arik <kadosh.arik@gmail.com>
2020-10-17 11:58:24 +0300

    added profile viewset

--- PyDjangoApi_App1/views.py
+++ PyDjangoApi_App1/views.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from PyDjangoApi_App1 import serializers
-
+from PyDjangoApi_App1 import models
+from rest_framework.authentication import TokenAuthentication
+from PyDjangoApi_App1 import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -94,3 +96,10 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
